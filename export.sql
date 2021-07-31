06cb6e7584ac5767ec2363780289671039e9cc9e
kaihusyahoo <kaihus@yahoo.com>
2021-07-31 10:47:13 -0700

    Add Author and update content

--- export.sql
+++ export.sql
@@ -4,15 +4,16 @@
 --------------------------------------------------------
 --  DDL for Function GETBOOKS
 --  Date: 2021.07.30
+--  Author: xxxx
 --------------------------------------------------------
 
-  CREATE OR REPLACE EDITIONABLE FUNCTION "HR"."GETBOOKS" (a CLOB) 
+CREATE OR REPLACE EDITIONABLE FUNCTION "HR"."GETBOOKS" (a CLOB) 
 RETURN BookSet_t
 AS
   l_tab  BookSet_t := BookSet_t();
 BEGIN
   l_tab.extend;
-  l_tab(l_tab.last) := Book_t('Kai','My Book', 'My Abstract');
+  l_tab(l_tab.last) := Book_t('John','My Book', 'My Abstract');
   RETURN l_tab;
 END;
 
